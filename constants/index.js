675d800426123da38a1d39a4d3147ea774f2f84a
neyshaa <88226939+neyshaa2626@users.noreply.github.com>
2023-07-27 15:49:40 +0330

    Update index.js
    Update Contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xd2472e84fbea833905af2e8282f4edc20f9a2514";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
